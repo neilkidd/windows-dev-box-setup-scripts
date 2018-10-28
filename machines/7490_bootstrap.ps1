4ddced707bab054dff4fafef554a889b50209877
neilkidd <neil.a.kidd@gmail.com>
2018-10-28 09:11:08 +0000

    Workaround choco / boxstarter cache path too long error
    https://github.com/chocolatey/boxstarter/issues/241

--- machines/7490_bootstrap.ps1
+++ machines/7490_bootstrap.ps1
@@ -23,64 +23,70 @@ function executeScript {
 	Invoke-Expression ((new-object net.webclient).DownloadString("$helperUri/$script"))
 }
 
+# Workaround choco / boxstarter path too long error
+# https://github.com/chocolatey/boxstarter/issues/241
+$ChocoCachePath = "$env:USERPROFILE\AppData\Local\Temp\chocolatey"
+New-Item -Path $ChocoCachePath -ItemType Directory -Force
+
 #--- Setting up Windows ---
 executeScript "FileExplorerSettings.ps1";
 executeScript "SystemConfiguration.ps1";
 executeScript "RemoveDefaultAppsExceptDell.ps1";
 
 # Browsers
-choco upgrade --yes googlechrome
+choco upgrade --cacheLocation="$ChocoCachePath" --yes googlechrome
 
-choco upgrade --yes Firefox --package-parameters="'l=en-GB'"
+choco upgrade --cacheLocation="$ChocoCachePath" --yes Firefox --package-parameters="'l=en-GB'"
 
-choco upgrade --yes lastpass
+choco upgrade --cacheLocation="$ChocoCachePath" --yes lastpass
 # end browsers
 
 # productivity tools
-choco upgrade --yes f.lux.install
-choco upgrade --yes 7zip.install
-choco upgrade --yes libreoffice-still
-choco upgrade --yes sumatrapdf.install
-choco upgrade --yes cutepdf
-choco upgrade --yes imgburn
-choco upgrade --yes paint.net
-choco upgrade --yes potplayer #Could have been vlc?
-choco upgrade --yes rufus
-choco upgrade --yes handbrake.install
-choco upgrade --yes chocolateygui
-choco upgrade --yes dropbox
-choco upgrade --yes speccy
-choco upgrade --yes virtualclonedrive
+choco upgrade --cacheLocation="$ChocoCachePath"--yes f.lux.install
+choco upgrade --cacheLocation="$ChocoCachePath" --yes 7zip.install
+choco upgrade --cacheLocation="$ChocoCachePath" --yes libreoffice-still
+choco upgrade --cacheLocation="$ChocoCachePath" --yes sumatrapdf.install
+choco upgrade --cacheLocation="$ChocoCachePath" --yes cutepdf
+choco upgrade --cacheLocation="$ChocoCachePath" --yes imgburn
+choco upgrade --cacheLocation="$ChocoCachePath" --yes paint.net
+choco upgrade --cacheLocation="$ChocoCachePath" --yes potplayer #Could have been vlc?
+choco upgrade --cacheLocation="$ChocoCachePath" --yes rufus
+choco upgrade --cacheLocation="$ChocoCachePath" --yes handbrake.install
+choco upgrade --cacheLocation="$ChocoCachePath" --yes chocolateygui
+choco upgrade --cacheLocation="$ChocoCachePath" --yes dropbox
+choco upgrade --cacheLocation="$ChocoCachePath" --yes speccy
+choco upgrade --cacheLocation="$ChocoCachePath" --yes virtualclonedrive
 
+# https://docs.microsoft.com/en-us/virtualization/hyper-v-on-windows/quick-start/enable-hyper-v
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 # executeScript "HyperV.ps1";
 RefreshEnv
 
 # command line dev tools
-choco upgrade --yes ag # ag is (grep || ack) on steroids
-choco upgrade --yes jq
-choco upgrade --yes curl
-choco upgrade --yes wget
-choco upgrade --yes vim
+choco upgrade --cacheLocation="$ChocoCachePath" --yes ag # ag is (grep || ack) on steroids
+choco upgrade --cacheLocation="$ChocoCachePath" --yes jq
+choco upgrade --cacheLocation="$ChocoCachePath" --yes curl
+choco upgrade --cacheLocation="$ChocoCachePath" --yes wget
+choco upgrade --cacheLocation="$ChocoCachePath" --yes vim
 
-choco upgrade --yes powershell-core
-choco upgrade --yes azure-cli
+choco upgrade --cacheLocation="$ChocoCachePath" --yes powershell-core
+choco upgrade --cacheLocation="$ChocoCachePath" --yes azure-cli
 Install-Module -Force Az
 # choco upgrade --yes microsoftazurestorageexplorer - fails checksum
 
 Install-Module -Force posh-git #for powershell integration
 
-choco upgrade --yes terraform
-choco upgrade --yes packer
+choco upgrade --cacheLocation="$ChocoCachePath" --yes terraform
+choco upgrade --cacheLocation="$ChocoCachePath" --yes packer
 # choco upgrade --yes vagrant - never completes :(
-choco upgrade --yes cmder
+choco upgrade --cacheLocation="$ChocoCachePath" --yes cmder
 RefreshEnv
 
 # UI tools
-choco upgrade --yes sysinternals
-choco upgrade --yes vscode
-choco upgrade --yes virtualbox
-choco upgrade --yes slack
+choco upgrade --cacheLocation="$ChocoCachePath" --yes sysinternals
+choco upgrade --cacheLocation="$ChocoCachePath" --yes vscode
+choco upgrade --cacheLocation="$ChocoCachePath" --yes virtualbox
+choco upgrade --cacheLocation="$ChocoCachePath" --yes slack
 
 #--- Tools ---
 #--- Installing VS and VS Code with Git
@@ -96,16 +102,16 @@ choco upgrade --yes slack
 # next line will do a full install. Beware: 22.64GB and 820 packages and timesout (2018-10-23)
 #choco install --yes visualstudio2017community --package-parameters="'--add Microsoft.VisualStudio.Component.Git --allWorkloads --includeRecommended --includeOptional --passive --locale en-US'"
 
-choco upgrade --yes visualstudio2017community --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
+choco upgrade --cacheLocation="$ChocoCachePath" --yes visualstudio2017community --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
 Update-SessionEnvironment #refreshing env due to Git install
 # See https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community?view=vs-2017
-choco upgrade --yes visualstudio2017-workload-azure 
-choco upgrade --yes visualstudio2017-workload-data
-choco upgrade --yes visualstudio2017-workload-manageddesktop
-choco upgrade --yes visualstudio2017-workload-netcoretools
-choco upgrade --yes visualstudio2017-workload-netweb 
+choco upgrade --cacheLocation="$ChocoCachePath" --yes visualstudio2017-workload-azure 
+choco upgrade --cacheLocation="$ChocoCachePath" --yes visualstudio2017-workload-data
+choco upgrade --cacheLocation="$ChocoCachePath" --yes visualstudio2017-workload-manageddesktop
+choco upgrade --cacheLocation="$ChocoCachePath" --yes visualstudio2017-workload-netcoretools
+choco upgrade --cacheLocation="$ChocoCachePath" --yes visualstudio2017-workload-netweb 
 
-choco upgrade --yes jetbrainstoolbox
+choco upgrade --cacheLocation="$ChocoCachePath" --yes jetbrainstoolbox
 #choco upgrade --yes resharper-ultimate-all --package-parameters="'/PerMachine /NoCpp /NoTeamCityAddin'"
 
 Enable-UAC
