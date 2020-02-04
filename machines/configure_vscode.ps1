e579cf879957845ecae80554031dc00062ba85f6
neilkidd <neil.a.kidd@gmail.com>
2020-02-04 16:41:53 +0000

    Initial version to configure vs code

---
+++ machines/configure_vscode.ps1
@@ -0,0 +1,33 @@
+# windows 10 specific.
+# expects code to installed and on path
+# Expects git bash to be on the path.
+
+code --install-extension AlanWalk.markdown-toc
+code --install-extension bungcip.better-toml
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension redhat.vscode-yaml #all the yamls
+
+# configure (user) settings.json
+
+$settingsDirPath = "$env:APPDATA\Code\User\"
+$settingsFilePath = "$settingsDirPath\settings.json"
+$utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+
+if (-not (Test-Path -LiteralPath $settingsDirPath)) {
+    "Creating chocoCachePath dir."
+    New-Item -Path $settingsDirPath -ItemType Directory -Force
+}
+
+$bashPath = (Get-Command bash.exe).Path.Replace("\", "\\")
+
+$jsonTemplate = @"
+{{
+    "editor.renderWhitespace": "all",
+    "editor.renderControlCharacters": true,
+    "terminal.integrated.shell.windows": "{0}",
+    "cSpell.language": "en-GB"
+}}
+"@ -f $bashPath
+
+[System.IO.File]::WriteAllLines($settingsFilePath, $jsonTemplate, $utf8NoBomEncoding)
