66847b488b89b53f2d9d8500f8db4f611b3516bf
neilkidd <neil.a.kidd@gmail.com>
2020-07-22 22:42:07 +0100

    delete blank lines

--- scripts/SystemConfiguration.ps1
+++ scripts/SystemConfiguration.ps1
@@ -1,4 +1,2 @@
-
-
 #--- Enable developer mode on the system ---
 Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
