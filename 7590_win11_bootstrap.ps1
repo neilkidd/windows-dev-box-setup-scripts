a354a82de83ee22dab47ca168779ed20287b43a6
Neil Kidd <neilkidd@users.noreply.github.com>
2024-10-24 20:34:16 +0100

    Configure registry setting for taskbar multimonitor

--- 7590_win11_bootstrap.ps1
+++ 7590_win11_bootstrap.ps1
@@ -27,16 +27,17 @@ $RegValues = (Get-ItemProperty -Path $StuckRects3Path).Settings
 $RegValues[8] = 3
 Set-ItemProperty -Path $StuckRects3Path -Name Settings -Value $RegValues
 
-# TRY LATER #--- File Explorer Settings ---
-#
+#--- File Explorer Settings ---
+# taskbar where window is open for multi-monitor
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+
 # will expand explorer to the actual folder you're in
 # Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
 #adds things back in your left pane like recycle bin
 # Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
 #opens PC to This PC, not quick access
 # Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
-#taskbar where window is open for multi-monitor
-# Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+
 
 #######################################################################################################################
 # Install apps
